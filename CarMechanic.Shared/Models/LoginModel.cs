0c4c3c47e925e57a53300d9e865cff8b1c8f475f
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-10-24 10:54:29 +0200

    feat(auth): implement login page and functionality
    - add Login.razor with email/password form and validation
    - add LoginModel.cs for user login properties
    - add login.css for styling the login form
    - update Navbar.razor to navigate to home on logout
    - change route definition in Home.razor to focus on the dashboard

---
+++ CarMechanic.Shared/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CarMechanic.Shared.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
