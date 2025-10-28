ba260801a7818dde3c8f93feb9fc8e153a44b064
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-10-28 15:34:55 +0100

    test: add unit tests for work hour estimation

---
+++ CarMechanic.Shared/Utils/CalculationHelper.cs
@@ -0,0 +1,41 @@
+﻿using CarMechanic.Shared.Enums;
+using CarMechanic.Shared.Models;
+
+namespace CarMechanic.Shared.Utils
+{
+    public static class CalculationHelper
+    {
+        public static double CalculateWorkHourEstimation(Job job)
+        {
+            double categoryHours = job.JobCategory switch
+            {
+                JobCategory.Chassis => 3,
+                JobCategory.Engine => 8,
+                JobCategory.Suspension => 6,
+                JobCategory.Brakes => 4,
+                _ => 0
+            };
+
+            int carAge = DateTime.Now.Year - job.VehicleYear.Year;
+            double ageMultiplier = carAge switch
+            {
+                var age when age >= 0 && age < 5 => 0.5,
+                var age when age >= 5 && age < 10 => 1,
+                var age when age >= 10 && age < 20 => 1.5,
+                _ => 2
+            };
+
+            double severityMultiplier = job.Severity switch
+            {
+                var s when s >= 1 && s <= 2 => 0.2,
+                var s when s >= 3 && s <= 4 => 0.4,
+                var s when s >= 5 && s <= 7 => 0.6,
+                var s when s >= 8 && s <= 9 => 0.8,
+                10 => 1,
+                _ => 0
+            };
+
+            return categoryHours * ageMultiplier * severityMultiplier;
+        }
+    }
+}
---
+++ CarMechanic.Tests/CalculationHelperTest.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarMechanic.Shared.Models;
+using CarMechanic.Shared.Enums;
+using CarMechanic.Shared.Utils;
+
+namespace CarMechanic.Tests
+{
+    public class CalculationHelperTest
+    {
+        [Fact]
+        public void Test_CalculateWorkHourEstimation()
+        {
+            // Arrange
+            var job = new Job
+            {
+                JobCategory = JobCategory.Engine, // motor: 8 óra
+                VehicleYear = DateTime.Now.AddYears(-2), // korszerű: szorzó 0.5
+                Severity = 7 // közepes súlyosság: szorzó 0.6
+            };
+            double expectedEstimation = 8 * 0.5 * 0.6;
+
+            // Act
+            var actualEstimation = CalculationHelper.CalculateWorkHourEstimation(job);
+
+            // Assert
+            Assert.Equal(expectedEstimation, actualEstimation);
+        }
+
+        [Theory]
+        [InlineData(JobCategory.Brakes, 7, 5, 4 * 1 * 0.6)] // Fék (4), 7 év (1), 5-ös súly. (0.6) = 2.4
+        [InlineData(JobCategory.Suspension, 15, 9, 6 * 1.5 * 0.8)] // Futómű (6), 15 év (1.5), 9-es súly. (0.8) = 7.2
+        [InlineData(JobCategory.Chassis, 25, 10, 3 * 2 * 1)] // Karosszéria (3), 25 év (2), 10-es súly. (1) = 6.0
+        public void Test_CalculateWorkHourEstimation_VariousInputs(
+            JobCategory category, int vehicleAge, int severity, double expectedEstimation
+        )
+        {
+            // Arrange
+            var job = new Job
+            {
+                JobCategory = category,
+                VehicleYear = DateTime.Now.AddYears(-vehicleAge),
+                Severity = severity
+            };
+
+            // Act
+            var actualEstimation = CalculationHelper.CalculateWorkHourEstimation(job);
+
+            // Assert
+            Assert.Equal(expectedEstimation, actualEstimation);
+        }
+
+        [Fact]
+        public void Test_CalculateWorkHourEstimation_UnknownCategory()
+        {
+            // Arrange
+            var job = new Job
+            {
+                JobCategory = (JobCategory)999, // Ismeretlen kategória
+                VehicleYear = DateTime.Now.AddYears(-10), // 10 év (1)
+                Severity = 5 // közepes súlyosság: szorzó 0.6
+            };
+            double expectedEstimation = 0 * 1 * 0.6; // Várható eredmény: 0
+
+            // Act
+            var actualEstimation = CalculationHelper.CalculateWorkHourEstimation(job);
+
+            // Assert
+            Assert.Equal(expectedEstimation, actualEstimation);
+        }
+    }
+}
