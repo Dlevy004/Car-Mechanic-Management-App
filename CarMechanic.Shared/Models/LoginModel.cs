efccc2ea7b4ed44c467907a496076be218a85a7f
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-10-30 14:08:27 +0100

    fix(auth): add validation to login form

--- CarMechanic.Shared/Models/LoginModel.cs
+++ CarMechanic.Shared/Models/LoginModel.cs
@@ -1,9 +1,15 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace CarMechanic.Shared.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "The email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is requierd.")]
         public string Password { get; set; } = string.Empty;
     }
 }
